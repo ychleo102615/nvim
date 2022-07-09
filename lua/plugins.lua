11097f04dfa27a1ab546b1a6ac2e78764894d0f5
ychleo102615 <leo102615@gmail.com>
2022-07-09 21:27:18 +0800

    feat: packer使用懸浮視窗

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,57 +1,59 @@
-local nocode = function() return vim.fn.exists'g:vscode' == 0; end
+return require('packer').startup {
+    function(use)
+        -- Packer can manage itself as an optional plugin
+        use 'wbthomason/packer.nvim';
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself as an optional plugin
-    use 'wbthomason/packer.nvim';
+        -- Color Scheme
+        use 'rebelot/kanagawa.nvim';
+        use 'sainnhe/sonokai';
+        use 'sainnhe/gruvbox-material';
+        use 'ellisonleao/gruvbox.nvim';
 
-    -- Color Scheme
-    use 'rebelot/kanagawa.nvim';
-    use 'sainnhe/sonokai';
-    use 'sainnhe/gruvbox-material';
-    use 'ellisonleao/gruvbox.nvim';
+        -- Tree Sitter
+        use {
+            'nvim-treesitter/nvim-treesitter',
+            run = ':TSUpdate'
+        };
+        use {'nvim-treesitter/nvim-treesitter-textobjects'};
 
-    -- Tree Sitter
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    };
-    use {'nvim-treesitter/nvim-treesitter-textobjects'};
+        -- Tree
+        use {
+            'kyazdani42/nvim-tree.lua',
+            requires = {
+              'kyazdani42/nvim-web-devicons', -- optional, for file icons
+            },
+            tag = 'nightly', -- optional, updated every week. (see issue #1193)
+        };
 
-    -- Tree
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-          'kyazdani42/nvim-web-devicons', -- optional, for file icons
-        },
-        tag = 'nightly', -- optional, updated every week. (see issue #1193)
-    };
+        -- buffer line
+        use {
+            'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons',
+        };
 
-    -- buffer line
-    use {
-        'akinsho/bufferline.nvim', requires = 'kyazdani42/nvim-web-devicons',
-    };
+        -- lsp config
+        use {
+            'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer',
+            "onsails/lspkind-nvim"
+        };
 
-    -- lsp config
-    use {
-        'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer',
-        "onsails/lspkind-nvim"
-    };
-
-    -- cmp, config from: https://gist.github.com/mengwangk/e5b64dbbeadc81b0129f274908a7b692
-    use {
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-nvim-lsp",
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-calc',
-            'hrsh7th/cmp-nvim-lua',
-            'hrsh7th/cmp-emoji',
-            --'quangnguyen30192/cmp-nvim-ultisnips',
-            --'octaltree/cmp-look',
-            'f3fora/cmp-spell',
-            --'SirVer/ultisnips',
-            "saadparwaiz1/cmp_luasnip"
+        -- cmp, config from: https://gist.github.com/mengwangk/e5b64dbbeadc81b0129f274908a7b692
+        use {
+            "hrsh7th/nvim-cmp",
+            requires = {
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-nvim-lsp",
+                'hrsh7th/cmp-path',
+                'hrsh7th/cmp-calc',
+                'hrsh7th/cmp-nvim-lua',
+                'hrsh7th/cmp-emoji',
+                'f3fora/cmp-spell',
+                "saadparwaiz1/cmp_luasnip"
+            }
+        };
+    end,
+    config = {
+        display = {
+            open_fn = require('packer.util').float,
         }
-    };
-end);
+    },
+};
