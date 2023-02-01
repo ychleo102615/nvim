eaad0074ca4b5bb67b89fd29b9979bfb33ca6688
ychleo102615 <leo102615@gmail.com>
2023-02-01 09:18:59 +0800

    feat: 刪除不需要的null-ls功能

--- lua/plugin-config/null-ls.lua
+++ lua/plugin-config/null-ls.lua
@@ -4,9 +4,7 @@ local null_ls = require("null-ls");
 -- Config list: https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
 null_ls.setup {
     sources = {
-        null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.completion.spell,
     },
 };
