3fb6293f74c2c9d9f46ba6b18a99b599d7ff81d5
ychleo102615 <leo102615@gmail.com>
2023-02-03 14:39:24 +0800

    feat: 調整nvim lsp環境設定

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,7 +1,10 @@
 require("mason").setup();
 require("mason-lspconfig").setup{
-    -- The following is server name
-    -- https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
+    --[[
+        Server List and theris githubs
+        https://github.com/williamboman/mason-lspconfig.nvim#available-lsp-servers
+        :help lspconfig-all or https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    ]]
     ensure_installed = {
         "sumneko_lua",
         "gopls",
@@ -14,6 +17,30 @@ require("mason-lspconfig").setup{
 
 local on_attach = require("keymap.lsp").on_attach;
 local lspconfig = require("lspconfig");
+-- require("tools.tool").echo(vim.api.nvim_get_runtime_file("", true));
+--[[
+    sumneko server config: https://github.com/sumneko/lua-language-server/wiki/Configuration-File
+]]
+local NVIM_ENV = "/.config/nvim";
+local function getNvimSetting()
+    if not vim.fn.getcwd():match(NVIM_ENV) then
+        return;
+    end
+    return {
+        Lua = {
+            runtime = { version = "LuaJIT" },
+            diagnostics = {
+                globals = {"vim"},
+            },
+            workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    };
+end
 -- :h mason-lspconfig-automatic-server-setup
 require("mason-lspconfig").setup_handlers {
     function (serverName)
@@ -22,20 +49,7 @@ require("mason-lspconfig").setup_handlers {
     ["sumneko_lua"] = function()
         lspconfig.sumneko_lua.setup {
             on_attach = on_attach,
-            settings = {
-                Lua = {
-                    runtime = { version = "LuaJIT" },
-                    diagnostics = {
-                        globals = {"vim"},
-                    },
-                    workspace = {
-                        library = vim.api.nvim_get_runtime_file("", true),
-                    },
-                    telemetry = {
-                        enable = false,
-                    },
-                },
-            },
+            settings  = getNvimSetting(),
         };
     end,
 };
