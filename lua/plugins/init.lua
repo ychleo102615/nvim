b90dcd3903feb8cb33928eddd7071b8c7124686b
ychleo102615 <leo102615@gmail.com>
2023-02-08 15:28:24 +0800

    fix: 修正有時候leap不啟用問題

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,7 +26,6 @@ return {
     -- motion
     {
         'ggandor/leap.nvim',
-        event = "VeryLazy",
         config = function()
             require('leap').add_default_mappings();
         end
