7df4d239fc9c562b119d6ed7943f9c7c11311b32
Huang Yao Chang <leo.huang@skylinetw.com>
2023-02-21 15:10:47 +0800

    feat: Add [Gitsigns] keymap desc

--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -22,20 +22,21 @@ local function on_attach(bufnr)
     end, { expr = true, desc = "Prev [C]Hunk" })
 
     -- Actions
-    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
-    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
+    map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>'       , { desc = "Stage Hunk" })
+    map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>'       , { desc = "Reset Hunk" })
+    map('n', '<leader>hS', gs.stage_buffer                         , { desc = "Stage Buffer" })
+    map('n', '<leader>hu', gs.undo_stage_hunk                      , { desc = "Undo Stage Hunk" })
+    map('n', '<leader>hR', gs.reset_buffer                         , { desc = "Reset Buffer" })
+    map('n', '<leader>hp', gs.preview_hunk                         , { desc = "Prview Hunk" })
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end , { desc = "Blame Line" })
+    map('n', '<leader>tb', gs.toggle_current_line_blame            , { desc = "Toggle Line Blame" })
+    map('n', '<leader>hd', gs.diffthis                             , { desc = "Diff This" })
+    map('n', '<leader>hD', function() gs.diffthis('~') end         , { desc = "Diff This With Head" })
+    map('n', '<leader>td', gs.toggle_deleted                       , { desc = "Toggle Deleted Hunk View" })
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = "Select Hunk" })
+    map('n', '<leader>ih', gs.select_hunk, { desc = "Select Hunk" })
 end
 
 return {
