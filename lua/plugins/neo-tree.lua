84e30896d5a072b34233b20298994af931c415c9
Huang Yao Chang <leo.huang@skylinetw.com>
2023-03-22 18:09:37 +0800

    feat: add neo tree config (suspended)

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,38 @@
+return {
+    -- Tree
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-tree/nvim-web-devicons',
+            'MunifTanjim/nui.nvim',
+        },
+        cond = false,
+        -- cond = not IS_USING_VSCODE,
+        opts = {
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- when true, they will just be displayed differently than normal items
+                    always_show = {
+                        -- remains visible even if other settings would normally hide it
+                        ".gitignored",
+                    },
+                    never_show = {
+                        -- remains hidden even if visible is toggled to true, this overrides always_show
+                        ".DS_Store",
+                    },
+                },
+            },
+        },
+        config = function(_, opts)
+            vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" });
+            vim.fn.sign_define("DiagnosticSignWarn",  { text = " ", texthl = "DiagnosticSignWarn" });
+            vim.fn.sign_define("DiagnosticSignInfo",  { text = " ", texthl = "DiagnosticSignInfo" });
+            vim.fn.sign_define("DiagnosticSignHint",  { text = "",  texthl = "DiagnosticSignHint" });
+            require("neo-tree").setup(opts);
+        end,
+        -- keys = {
+        --     { "<leader>d",  "<cmd>NeoTreeRevealToggle<cr>",  desc = "Toggle Tree [D]irectory" },
+        -- }
+    },
+};
