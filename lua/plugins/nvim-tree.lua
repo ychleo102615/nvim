73f9973a3f70324a1546156ab46976386b8716fe
ychleo102615 <leo102615@gmail.com>
2023-04-28 10:15:58 +0800

    feat: substitute view mappinigs with on_attach

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,19 +8,18 @@ return {
         cond = not IS_USING_VSCODE,
         opts = {
             sort_by = "case_sensitive",
-            view = {
-                mappings = {
-                    list = {
-                        { key = "<C-e>", action = "" },
-                    },
-                },
-            },
             filters = {
                 exclude  = { "node_modules" },
             },
             git = {
                 ignore = false,
             },
+            on_attach = function (bufnr)
+                -- from help "nvim-tree-mappings-default"
+                local api = require('nvim-tree.api');
+                api.config.mappings.default_on_attach(bufnr);
+                vim.keymap.del('n', '<C-e>', { buffer = bufnr });
+            end,
         },
         keys = {
             { "<leader>d", "<cmd>NvimTreeFindFileToggle<cr>", desc = "Toggle Tree [D]irectory" },
