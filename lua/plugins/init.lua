2c98b2298d5d9c4ba8c8d5326760d0712c52c45b
ychleo102615 <leo102615@gmail.com>
2023-07-03 14:55:11 +0800

    feat: enable leap backdrop after colorscheme set

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -21,6 +21,11 @@ return {
         'ggandor/leap.nvim',
         config = function()
             require('leap').add_default_mappings();
+            vim.api.nvim_create_autocmd('ColorScheme', {
+                callback = function()
+                    vim.api.nvim_set_hl(0, 'LeapBackdrop', { fg = 'grey' })
+                end,
+            });
         end
     },
     {
