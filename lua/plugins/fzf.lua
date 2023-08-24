b6c37c4c0359f5e3b5b675a7a96dc860406d2b74
ychleo102615 <leo102615@gmail.com>
2023-08-24 17:01:10 +0800

    feat: ajust fzf config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,11 +5,11 @@ return {
             {
                 "junegunn/fzf",
                 build = ":call fzf#install()",
-                keys = {
-                    { "<leader>fz", "<cmd>Files<cr>", mode = {"n"}, desc = "fzf Files" },
-                    { "<leader>fg", "<cmd>Rg<cr>",    mode = {"n"}, desc = "fzf Rg" },
-                },
             },
+        },
+        keys = {
+            { "<leader>fz", "<cmd>Files<cr>", mode = {"n"}, desc = "fzf Files" },
+            { "<leader>fg", "<cmd>Rg<cr>",    mode = {"n"}, desc = "fzf Rg" },
         },
     },
 };
