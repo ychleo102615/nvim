5b2b51728638d7e863e15fb7b7a26eb73df4d4e8
ychleo102615 <leo102615@gmail.com>
2023-08-29 15:59:27 +0800

    feat: add Copilot keymap and settings

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,13 @@
+if IS_USING_VSCODE then
+    return;
+end
 return {
     "github/copilot.vim",
     event = "BufReadPost",
+    keys = {
+        {'<C-D>', '<Plug>(copilot-dismiss)',    desc = 'Copilot Dismiss',  mode = 'i'},
+        {'<C-]>',   '<Plug>(copilot-next)',     desc = 'Copilot Next',     mode = 'i'},
+        {'<C-[>',   '<Plug>(copilot-previous)', desc = 'Copilot Previous', mode = 'i'},
+        {'<C-\\>',  '<Plug>(copilot-suggest)',  desc = 'Copilot Suggest',  mode = 'i'},
+    },
 };
