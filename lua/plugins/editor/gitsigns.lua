be2e261d560d62313eaf24fdd70bae9ce360301b
Huang Yao Chang <leo.huang@skylinetw.com>
2024-10-11 15:48:46 +0800

    fix: git sign error

--- lua/plugins/editor/gitsigns.lua
+++ lua/plugins/editor/gitsigns.lua
@@ -49,11 +49,20 @@ return {
         event = "BufReadPre",
         opts = {
             signs = {
-                add          = { hl = "GitSignsAdd",    text = "▎",  numhl = "GitSignsAddNr",    linehl = "GitSignsAddLn"    },
-                change       = { hl = "GitSignsChange", text = "▎",  numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-                delete       = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-                topdelete    = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-                changedelete = { hl = "GitSignsChange", text = "▎",  numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+                add          = { text = "▎",  },
+                change       = { text = "▎",  },
+                delete       = { text = "契", },
+                topdelete    = { text = "契", },
+                changedelete = { text = "▎",  },
+                untracked    = { text = '┆',  },
+            },
+            signs_staged = {
+                add          = { text = '┃' },
+                change       = { text = '┃' },
+                delete       = { text = '_' },
+                topdelete    = { text = '‾' },
+                changedelete = { text = '~' },
+                untracked    = { text = '┆' },
             },
             current_line_blame = true,
             current_line_blame_opts = {
