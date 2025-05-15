16f0368f7233a3ecd024236db8301afeb26250b4
Huang Yao Chang <leo.huang@skylinetw.com>
2025-05-15 10:19:16 +0800

    feat: remove unused

--- lua/keymap/lsp.lua
+++ lua/keymap/lsp.lua
@@ -1,12 +1,5 @@
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/lsp/keymaps.lua
 -- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/lsp/format.lua
-local function isNullLsSupported()
-    local buf = vim.api.nvim_get_current_buf();
-    local ft = vim.bo[buf].filetype;
-    local have_nls = #require("null-ls.sources").get_available(ft, "NULL_LS_FORMATTING") > 0;
-    return have_nls;
-end
-
 return {
     setupKeymap = function()
         -- Mappings.
